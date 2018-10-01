f93a8e4ef510787164d601e3ca90e97a4b2aab02
GyroPhan <khanhhung.dev@gmail.com>
2018-10-01 16:26:12 +0700

    layout baoinfo

--- Money/Main/ThuChi.js
+++ Money/Main/ThuChi.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react';
 import {
     View, ScrollView,
     Text, Image,
-    TextInput, TouchableOpacity, Switch
+    TextInput, TouchableOpacity, Switch,
+    FlatList,
 } from 'react-native';
 
 const ADD = require('../Image/ADD.png');
 
 export default class ThuChi extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            datathu: [
+                { nguonThu: 'luong JT', tienThu: '8000' },
+                { nguonThu: 'kinh doanh', tienThu: '2000' },
+                { nguonThu: 'chay grab', tienThu: '1000' },
+            ],
+            datachi: [
+                { nguonChi: 'tra no MAC', tienChi: '2000' },
+                { nguonChi: 'me', tienChi: '2000' },
+                { nguonChi: 'ngoc', tienChi: '1000' },
+            ],
 
+        }
+    }
+    renderItemThu = ({ item, index }) => {
+        return (
+            <View style={ao.baorender}>
+                <View style={ao.baorenderinfo}>
+                    <Text style={ao.chuinfo}>{item.nguonThu}</Text>
+                </View>
+                <View style={ao.baorendertien}>
+                    <Text style={ao.chutien}>{item.tienThu}</Text>
+                </View>
+            </View>
+        )
+    }
+    renderItemChi = ({ item, index }) => {
+        return (
+            <View style={ao.baorender}>
+                <View style={ao.baorenderinfo}>
+                    <Text style={ao.chuinfo}>{item.nguonChi}</Text>
+                </View>
+                <View style={ao.baorendertien}>
+                    <Text style={ao.chutien}>{item.tienChi}</Text>
+                </View>
+            </View>
+        )
+    }
     render() {
         return (
             <View style={ao.baolon}>
@@ -32,38 +72,30 @@ export default class ThuChi extends Component {
                         </View>
 
                         <TouchableOpacity style={ao.nut}>
-                            <Image 
-                            style={{width:50, height:50}}
-                            source={ADD}/>
+                            <Image
+                                style={{ width: 50, height: 50 }}
+                                source={ADD} />
                         </TouchableOpacity>
                     </View>
                 </View>
 
-                <View style={{flex:5}}>
-                    <ScrollView style={ao.baoinfo1}>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                     
+                <View style={{ flex: 5 }}>
+                    <ScrollView style={ao.baoinfo}>
+                        <FlatList
+                            data={this.state.datathu}
+                            renderItem={this.renderItemThu}
+                            keyExtractor={(item, index) => index.toString()}
+                        >
+                        </FlatList>
                     </ScrollView>
-                    <ScrollView style={ao.baoinfo2}>
-                    <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
-                        <Text> INFO</Text>
+
+                    <ScrollView style={ao.baoinfo}>
+                        <FlatList
+                            data={this.state.datachi}
+                            renderItem={this.renderItemChi}
+                            keyExtractor={(item, index) => index.toString()}
+                        >
+                        </FlatList>
                     </ScrollView>
                 </View>
 
@@ -83,7 +115,7 @@ const ao = {
     baonhap: {
         flex: 1.5,
     },
-  
+
     baoketqua: {
         flex: 0.7,
         backgroundColor: '#9ACD32',
@@ -103,7 +135,7 @@ const ao = {
 
     },
     input2: {
-        flex: 1,      
+        flex: 1,
         margin: 5,
         padding: 5,
         borderWidth: 2,
@@ -113,8 +145,6 @@ const ao = {
     baonut: {
         flex: 1,
         flexDirection: 'row',
-
-
     },
     nut: {
         flex: 1,
@@ -129,17 +159,27 @@ const ao = {
         alignItems: 'center',
         flexDirection: 'row',
     },
-    baoinfo1:{
-        flex:1,
+    baoinfo: {
+        flex: 1,
         borderWidth: 1,
         borderColor: 'black',
-        margin:5,
+        margin: 5,
     },
-    baoinfo2:{
-        flex:1,
-        borderWidth: 1,
-        borderColor: 'black',
-        margin:5,
-
+    baorender: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 5
+    },
+    baorenderinfo: {
+        flex: 4.5,
+    },
+    baorendertien: {
+        flex: 1,
+    },
+    chuinfo: {
+        fontSize: 20,
+    },
+    chutien: {
+        fontSize: 20,
     },
 }
