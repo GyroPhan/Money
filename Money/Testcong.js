92eecf44ce19816d11c954be564ff428dae2732a
GyroPhan <khanhhung.dev@gmail.com>
2018-10-05 10:40:47 +0700

    finish tinhtoan

--- Money/Testcong.js
+++ Money/Testcong.js
@@ -10,24 +10,37 @@ export default class Testcong extends Component {
                 { nguonThu: 'kinh doanh', tienThu: '2000' },
                 { nguonThu: 'chay grab', tienThu: '1000' },
             ],
+            array1: [1, 2, 3, 4, 7],
             s1: '',
             s2: '',
+            newar: [],
+            sum:'',
         }
     }
     tinhtoan1 = () => {
-        const array1 = [1, 2, 3, 4, 7];
         const cac = (a, b) => a + b;
-
         // 1 + 2 + 3 + 4
-        this.state.s1 = array1.reduce(cac)
-        console.log(array1.reduce(cac));
+        this.state.s1 = this.state.array1.reduce(cac)
+        console.log(this.state.s1);
         // expected output: 10
 
         // 5 + 1 + 2 + 3 + 4
-        this.state.s2 = array1.reduce(cac, 5)
-        console.log(array1.reduce(cac, 5));
+        this.state.s2 = this.state.array1.reduce(cac, 5)
+        console.log(this.state.s2);
         // expected output: 15
 
+    }
+    taomang() {
+
+        this.state.datathu.map((item, index) => {
+            console.log('map', this.state.datathu[index].tienThu)
+
+            this.state.newar.push(Number(this.state.datathu[index].tienThu))
+            console.log('push', this.state.newar)
+
+        const cac = (a, b) => a + b
+            this.state.sum= this.state.newar.reduce(cac)
+        })
     }
 
 
@@ -35,8 +48,8 @@ export default class Testcong extends Component {
         return (
             <View style={{ marginTop: 20 }}>
                 <Text>Testcong</Text>
-                {this.tinhtoan1()}
-                <Text>{this.state.s1}</Text>
+                {this.taomang()}
+                <Text>{this.state.sum}</Text>
                 <Text>{this.state.s2}</Text>
             </View>
         );
